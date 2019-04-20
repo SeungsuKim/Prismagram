bc602a8f62b9f9b5cec5ac42a64f442a7ba0201f
seungsukim <seungsu0407@gmail.com>
2019-04-20 23:05:59 +0900

    #2.4 Resolvers with Prisma

--- src/server.js
+++ src/server.js
@@ -9,4 +9,6 @@ const server = new GraphQLServer({ schema });
 
 server.express.use(logger("dev"));
 
-server.start({ port: PORT }, () => console.log(`Server running on port ${PORT}`));
+server.start({ port: PORT }, () =>
+  console.log(`✅  Server running on port ${PORT}`)
+);

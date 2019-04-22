aec676d396f4e11ef532567001e476ed5b90bbde
seungsukim <seungsu0407@gmail.com>
2019-04-23 01:23:41 +0900

    #3.24 sendMessage Resolver

---
+++ src/api/Message/sendMessage/sendMessage.js
@@ -0,0 +1,48 @@
+import { RoboMaker } from "aws-sdk";
+import { prisma } from "../../../../generated/prisma-client";
+import { ROOM_FRAGMENT } from "../../../fragment";
+
+export default {
+  Mutation: {
+    sendMessage: async (_, args, { request, isAuthenticated }) => {
+      isAuthenticated(request);
+      const { roomId, toId, message } = args;
+      const { user } = request;
+      let room;
+      if (roomId === undefined) {
+        if (user.id !== toId) {
+          room = await prisma.createRoom({
+            participants: {
+              connect: [{ id: toId }, { id: user.id }]
+            }
+          }).$fragment(ROOM_FRAGMENT);
+        }
+      } else {
+        room = await prisma.room({ id: roomId }).$fragment(ROOM_FRAGMENT);
+      }
+      if (!room) {
+        throw Error("Room not found");
+      }
+      const to = room.participants.filter(
+        participant => participant.id !== user.id
+      )[0]
+      const newMessage = await prisma.createMessage({
+        text: message,
+        from: {
+          connect: { id: user.id }
+        },
+        to: {
+          connect: {
+            id: room ? to.id : toId
+          }
+        },
+        room: {
+          connect: {
+            id: room.id
+          }
+        }
+      });
+      return newMessage;
+    }
+  }
+}
